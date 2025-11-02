2d0922c103e340e919b63c8da5e6ba835a3e8821
Ilya Gellert <gellertilya@gmail.com>
2025-11-03 01:06:00 +0300

    feat(model): add Comment, it's structure to represents a comment entity in the system

--- internal/domain/model/comment.go
+++ internal/domain/model/comment.go
@@ -1,1 +1,26 @@
 package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Comment represents a comment entity in the system.
+type Comment struct {
+	ID        uuid.UUID
+	ParentID  *uuid.UUID
+	Text      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// IsRoot checks if the comment is a root comment (i.e., it has no parent).
+func (c *Comment) IsRoot() bool {
+	return c.ParentID == nil
+}
+
+// IsDeleted checks if the comment has been deleted.
+func (c *Comment) IsDeleted() bool {
+	return c.Text == "[deleted]"
+}
